fca378dca6e6ab10e0c19017011c94eb929e4976
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2018-04-05 14:54:25 +0200

    Renamed project to 'mediate' (4)

--- mediate/settings.py
+++ mediate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gqj=#)n_cf6@98fk-e)v_6g#)ifng!(6y%fxa=__b6b-ly@^sf'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['131.174.132.80']
 
@@ -60,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'catalogues.urls'
+ROOT_URLCONF = 'mediate.urls'
 
 TEMPLATES = [
     {
@@ -78,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'catalogues.wsgi.application'
+WSGI_APPLICATION = 'mediate.wsgi.application'
 
 
 # Database
@@ -89,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'USER': 'mediate_admin',
         'NAME': 'mediate_book_items',
-        'PASSWORD': 'montoyahulsbosch',
+        'PASSWORD': config('DB_PASSWORD'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
