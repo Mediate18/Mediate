92a33033bebeb42ce9dbe873d3707835a265f407
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2018-04-06 09:01:48 +0200

    Made some imports relative

--- items/moderator.py
+++ items/moderator.py
@@ -1,6 +1,6 @@
 from moderation import moderation
 
-from items.models import Item, Publisher
+from .models import Item, Publisher
 
 moderation.register(Item)  # Uses default moderation settings
 moderation.register(Publisher)  # Uses default moderation settings
--- items/views/views.py
+++ items/views/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 
-from items.models import Lot, Item
+from ..models import Lot, Item
 
 
 # Catalogue detail views
