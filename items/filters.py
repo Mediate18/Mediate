8d0ee98e7240324b8111eb7b087b7e3b54277438
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2018-04-12 09:57:40 +0200

    No __init__ necessary for Django Filter.

--- items/filters.py
+++ items/filters.py
@@ -13,12 +13,3 @@ class LanguageFilter(django_filters.FilterSet):
     class Meta:
         model = Language
         fields = "__all__"
-
-    def __init__(self, data=None, queryset=None, prefix=None, strict=None, request=None):
-        queryset = Language.unmoderated_objects.all()
-        super(LanguageFilter, self).__init__(data=data, queryset=queryset, prefix=prefix, strict=strict, request=request)
-
-    # @property
-    # def qs(self):
-    #     self.queryset = Language.unmoderated_objects.all()
-    #     return super(LanguageFilter, self).qs
