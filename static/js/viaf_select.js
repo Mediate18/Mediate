df5267d5b4da552e84c01863795a365f0b3ffa50
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2018-05-28 09:55:05 +0200

    In the Work create form the title is now copied from the VIAF search box.

--- static/js/viaf_select.js
+++ static/js/viaf_select.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-        /* If there is a VIAF search box, show a link when a selection is made. */
+        // For VIAF search box, ...
         $('#id_viaf_id').change(function() {
+            // ... show a link when a selection is made;
             $('#viaf_uri').attr('href', $(this).val());
             $('#viaf_uri').html($(this).val());
+
+            // ... copy the title to the Title field;
+            var title = $('#id_viaf_id option').last().text()
+            $('#id_title').val(title)
         });
 });
