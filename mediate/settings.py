bef76878dc7c01bd96d6feeac318d6b62736805a
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2018-06-14 16:53:45 +0200

    Made ALLOWED_HOST instance specific.

--- mediate/settings.py
+++ mediate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['131.174.132.80']
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 
 # Application definition
