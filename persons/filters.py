f6783555e8889f6ef09c1cd77460fa155cf33d3f
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2018-06-22 14:18:47 +0200

    Improved Person list filter.

--- persons/filters.py
+++ persons/filters.py
@@ -1,12 +1,48 @@
 import django_filters
 from .models import *
+from viapy.api import ViafAPI
+from django.utils.translation import ugettext_lazy as _
 
 
 # Person filter
 class PersonFilter(django_filters.FilterSet):
+    from django_select2.forms import Select2MultipleWidget
+    short_name = django_filters.Filter(lookup_expr='icontains')
+    first_names = django_filters.Filter(lookup_expr='icontains')
+    surname = django_filters.Filter(lookup_expr='icontains')
+    sex = django_filters.MultipleChoiceFilter(
+        choices=Person.SEX_CHOICES,
+        widget=Select2MultipleWidget(attrs={'data-placeholder': "Select multiple"},)
+    )
+    city_of_birth = django_filters.ModelMultipleChoiceFilter(
+        queryset=Place.objects.all(),
+        widget=Select2MultipleWidget(attrs={'data-placeholder': "Select multiple"},)
+    )
+    city_of_death = django_filters.ModelMultipleChoiceFilter(
+        queryset=Place.objects.all(),
+        widget=Select2MultipleWidget(attrs={'data-placeholder': "Select multiple"},)
+    )
+    viaf_id = django_filters.Filter(method='viaf_id_filter')
+
     class Meta:
         model = Person
-        fields = "__all__"
+        exclude = ['uuid']
+        fields = [
+            'short_name',
+            'first_names',
+            'surname',
+            'sex',
+            'city_of_birth',
+            'date_of_birth',
+            'city_of_death',
+            'date_of_death',
+            'viaf_id'
+        ]
+
+    def viaf_id_filter(self, queryset, name, value):
+        if value:
+            return queryset.filter(viaf_id=ViafAPI.uri_base+"/"+value)
+        return queryset
 
 
 # PersonPersonRelation filter
