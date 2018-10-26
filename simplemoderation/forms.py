a013361759b58c08b185f3268e3aafa9cda91b03
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2018-10-26 12:08:22 +0200

    Changed the Moderation state radio widget to a toggle widget.

--- simplemoderation/forms.py
+++ simplemoderation/forms.py
@@ -3,15 +3,27 @@ from .models import *
 
 
 class ModerationModelForm(forms.ModelForm):
-    state = forms.CharField(
-        max_length=1,
-        widget=forms.RadioSelect(choices=[(tag.value, tag.name) for tag in
-                                          (ModerationState.APPROVED, ModerationState.REJECTED)])
-    )
+    approved = forms.BooleanField(label="State", required=False,
+                                  widget=forms.CheckboxInput(attrs={'data-toggle': 'toggle', 'data-on': 'Approved',
+                                                                    'data-off': 'Rejected', 'data-onstyle': 'success',
+                                                                    'data-offstyle': 'danger'}))
 
     class Meta:
         model = Moderation
-        fields = ['state', 'reason']
+        fields = ['approved', 'reason']
 
     class Media:
-        js = ('js/diff.js',)
+        js = ('js/diff.js', 'https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js')
+        css = {
+            'all': ('https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css',)
+        }
+
+    def save(self, commit=True):
+        """
+        Turns the 'approved' boolean field into a correct state
+        :param commit: 
+        :return: 
+        """
+        self.instance.state = ModerationState.APPROVED.value if self['approved'].value() \
+                                                                else ModerationState.REJECTED.value
+        return super().save(commit)
