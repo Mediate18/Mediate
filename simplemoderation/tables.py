aa83359992bb7c70af63e959255e5185fcf32d40
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2018-10-29 10:06:12 +0100

    The rendering of the Moderate column in the moderation table is now calculated dynamically per request.

--- simplemoderation/tables.py
+++ simplemoderation/tables.py
@@ -1,19 +1,18 @@
 import django_tables2 as tables
-from django_tables2.utils import A  # alias for Accessor
+from django.utils.html import format_html
+from django.urls import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
 from .models import *
 
 
 # Moderation table
 class ModerationTable(tables.Table):
-    id = tables.LinkColumn('change_moderation', text='Moderate', args=[A('pk')], orderable=False, verbose_name="Moderate",
-                           empty_values=[moderation.id for moderation in
-                                         Moderation.objects.filter(state__in=[ModerationState.APPROVED.value,
-                                                                              ModerationState.REJECTED.value])])
+    id = tables.Column(orderable=False, verbose_name=_("Moderate"))
 
     class Meta:
         model = Moderation
         attrs = {'class': 'table table-sortable'}
-        fields = ('editor', 'created_datetime', 'action', 'content_type', 'data', 'state', 'id',)
+        fields = ('editor', 'created_datetime', 'action', 'content_type', 'data', 'master', 'state', 'reason', 'id',)
 
     def render_action(self, record):
         return ModerationAction(record.action).name.capitalize()
@@ -24,3 +23,11 @@ class ModerationTable(tables.Table):
     def render_state(self, record):
         return ModerationState(record.state).name.capitalize()
 
+    def render_id(self, record):
+        moderation = record
+        if moderation.state == ModerationState.PENDING.value and \
+                (moderation.master is None or moderation.master.state != ModerationState.PENDING.value):
+            return format_html('<a title="{}" href="{}"><span class="glyphicon glyphicon-pencil"></a>'
+                               .format(_("Moderate"), reverse_lazy('change_moderation', args=[record.id])))
+        else:
+            return ""
