5645256743567922dfffeb6426efcb5e4836d2fc
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2018-11-14 11:59:55 +0100

    Made all moderation funcionality depend on a setting MODERATION_OFF.

--- simplemoderation/tools.py
+++ simplemoderation/tools.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.utils.translation import ugettext_lazy as _
 
+from django.conf import settings
+
 from .models import Moderation, ModerationAction, ModerationState, register_model
 
 
@@ -44,69 +46,69 @@ def moderate(action=None, check_under_moderation=True):
         :param cls: the View class
         :return: the extende View class
         """
+        if not settings.MODERATION_OFF:
+            if DeleteView in cls.__bases__:
+                moderation_action = action if action else ModerationAction.DELETE
 
-        if DeleteView in cls.__bases__:
-            moderation_action = action if action else ModerationAction.DELETE
-
-            def delete(self, request, *args, **kwargs):
-                if not request.user.is_superuser:
-                    self.object = self.get_object()
-                    if self.object.under_moderation():
-                        messages.add_message(self.request, messages.WARNING,
-                                             _("This object ({}) is already under moderation.").format(self.object))
-                    else:
-                        moderation = Moderation.create(editor=self.request.user, obj=self.object,
-                                                       action=moderation_action)
-                        moderation.save()
-                        messages.add_message(self.request, messages.SUCCESS,
-                                             _("Your changes will be sent to a moderator for reviewing."))
-                    return HttpResponseRedirect(self.get_success_url())
-                else:
-                    return super().delete(request, *args, **kwargs)
-
-            cls.delete = delete
-
-        else:
-            if action:
-                moderation_action = action
-            elif CreateView in cls.__bases__:
-                moderation_action = ModerationAction.CREATE
-            elif UpdateView in cls.__bases__:
-                moderation_action = ModerationAction.UPDATE
-            else:
-                raise Exception(_("Could not determine moderation action "))
-
-            if UpdateView in cls.__bases__:
-                # Check whether the object is under moderation when the update form is requested
-                if check_under_moderation:
-                    def get(self, request, *args, **kwargs):
-                        obj = self.get_object()
-                        if obj.under_moderation():
+                def delete(self, request, *args, **kwargs):
+                    if not request.user.is_superuser:
+                        self.object = self.get_object()
+                        if self.object.under_moderation():
                             messages.add_message(self.request, messages.WARNING,
-                                                 _("This object ({}) is already under moderation.").format(obj))
-                            return HttpResponseRedirect(self.get_success_url())
+                                                 _("This object ({}) is already under moderation.").format(self.object))
                         else:
-                            return super(cls, self).get(request, *args, **kwargs)
-
-                    cls.get = get
-
-            # Create a form_valid method for both Create and Update
-            def form_valid(self, form, *args, **kwargs):
-                if not self.request.user.is_superuser:
-                    self.object = form.save(commit=False)
-                    if self.object.under_moderation():
-                        messages.add_message(self.request, messages.WARNING,
-                                             _("This object ({}) is already under moderation.").format(self.object))
+                            moderation = Moderation.create(editor=self.request.user, obj=self.object,
+                                                           action=moderation_action)
+                            moderation.save()
+                            messages.add_message(self.request, messages.SUCCESS,
+                                                 _("Your changes will be sent to a moderator for reviewing."))
+                        return HttpResponseRedirect(self.get_success_url())
                     else:
-                        moderation = Moderation.create(editor=self.request.user, obj=self.object, action=moderation_action)
-                        moderation.save()
-                        messages.add_message(self.request, messages.SUCCESS,
-                                             _("Your changes will be sent to a moderator for reviewing."))
-                    return HttpResponseRedirect(self.get_success_url())
-                else:
-                    return super().form_valid(form, *args, **kwargs)
+                        return super().delete(request, *args, **kwargs)
 
-            cls.form_valid = form_valid
+                cls.delete = delete
+
+            else:
+                if action:
+                    moderation_action = action
+                elif CreateView in cls.__bases__:
+                    moderation_action = ModerationAction.CREATE
+                elif UpdateView in cls.__bases__:
+                    moderation_action = ModerationAction.UPDATE
+                else:
+                    raise Exception(_("Could not determine moderation action "))
+
+                if UpdateView in cls.__bases__:
+                    # Check whether the object is under moderation when the update form is requested
+                    if check_under_moderation:
+                        def get(self, request, *args, **kwargs):
+                            obj = self.get_object()
+                            if obj.under_moderation():
+                                messages.add_message(self.request, messages.WARNING,
+                                                     _("This object ({}) is already under moderation.").format(obj))
+                                return HttpResponseRedirect(self.get_success_url())
+                            else:
+                                return super(cls, self).get(request, *args, **kwargs)
+
+                        cls.get = get
+
+                # Create a form_valid method for both Create and Update
+                def form_valid(self, form, *args, **kwargs):
+                    if not self.request.user.is_superuser:
+                        self.object = form.save(commit=False)
+                        if self.object.under_moderation():
+                            messages.add_message(self.request, messages.WARNING,
+                                                 _("This object ({}) is already under moderation.").format(self.object))
+                        else:
+                            moderation = Moderation.create(editor=self.request.user, obj=self.object, action=moderation_action)
+                            moderation.save()
+                            messages.add_message(self.request, messages.SUCCESS,
+                                                 _("Your changes will be sent to a moderator for reviewing."))
+                        return HttpResponseRedirect(self.get_success_url())
+                    else:
+                        return super().form_valid(form, *args, **kwargs)
+
+                cls.form_valid = form_valid
 
         return cls
     return decorator
@@ -114,7 +116,7 @@ def moderate(action=None, check_under_moderation=True):
 
 class ModeratedCreateView(CreateView):
     def form_valid(self, form, *args, **kwargs):
-        if not self.request.user.is_superuser:
+        if not self.request.user.is_superuser and not settings.MODERATION_OFF:
             self.object = form.save(commit=False)
             if self.object.under_moderation():
                 messages.add_message(self.request, messages.WARNING,
