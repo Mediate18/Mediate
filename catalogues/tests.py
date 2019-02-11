396e4a0543a079960d6b2dc991d37ef1ea0db31f
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2019-02-11 17:03:00 +0100

    Added the remaining view test for catalogues.

--- catalogues/tests.py
+++ catalogues/tests.py
@@ -142,5 +142,139 @@ class CatalogueHeldByTests(GenericCRUDTestMixin, TestCase):
         }
 
     def test_Detail(self):
-        """TODO: add CatalogueHeldByTests detail template"""
+        """TODO: add CatalogueHeldBy detail template"""
         pass
+
+
+class LotTests(GenericCRUDTestMixin, TestCase):
+    model = Lot
+
+    def get_add_form_data(self):
+        category = Category.objects.first()
+        catalogue = category.catalogue
+        return {
+            'catalogue_id': catalogue.pk,  # TODO: find out why the '_id' and '.pk' are necessary
+            'number_in_catalogue': 1,
+            'page_in_catalogue': 1,
+            'sales_price': '10 gulden',
+            'lot_as_listed_in_catalogue': 'lot_as_listed_in_catalogue test',
+            'index_in_catalogue': 1,
+            'category': category
+        }
+
+    def get_change_form_data(self):
+        return {
+            'number_in_catalogue': 2
+        }
+
+
+class PersonCollectionRelationTests(GenericCRUDTestMixin, TestCase):
+    model = PersonCollectionRelation
+
+    def get_add_form_data(self):
+        person = Person.objects.first()
+        collection, created = Collection.objects.get_or_create(**CollectionTests().get_add_form_data())
+        return {
+            'person': person,
+            'collection': collection
+        }
+
+    def get_change_form_data(self):
+        person = Person.objects.all()[1]
+        return {
+            'person': person
+        }
+
+    def test_Detail(self):
+        """TODO: add a PersonCollectionRelation detail template"""
+        pass
+
+
+class PersonCatalogueRelationRoleTests(GenericCRUDTestMixin, TestCase):
+    model = PersonCatalogueRelationRole
+
+    def get_add_form_data(self):
+        return {
+            'name': 'name test'
+        }
+
+    def get_change_form_data(self):
+        return {
+            'name': 'name test2'
+        }
+
+    def test_Detail(self):
+        """TODO: add a PersonCatalogueRelationRole detail template"""
+        pass
+
+
+class PersonCatalogueRelationTests(GenericCRUDTestMixin, TestCase):
+    model = PersonCatalogueRelation
+
+    def get_add_form_data(self):
+        person = Person.objects.first()
+        catalogue, created = Catalogue.objects.get_or_create(**CatalogueTests().get_add_form_data())
+        role, created = PersonCatalogueRelationRole.objects.get_or_create(**PersonCatalogueRelationRoleTests().get_add_form_data())
+        return {
+            'person': person,
+            'catalogue': catalogue,
+            'role': role
+        }
+
+    def get_change_form_data(self):
+        person = Person.objects.all()[1]
+        return {
+            'person': person
+        }
+
+    def test_Detail(self):
+        """TODO: add a PersonCatalogueRelation detail template"""
+        pass
+
+
+class ParisianCategoryTests(GenericCRUDTestMixin, TestCase):
+    model = ParisianCategory
+    url_names = {'list': 'parisiancategories'}
+
+    def get_add_form_data(self):
+        return {
+            'name': 'name test',
+            'description': 'description test'
+        }
+
+    def get_change_form_data(self):
+        return {
+            'name': 'name test2',
+            'description': 'description test2'
+        }
+
+    def test_Detail(self):
+        """TODO: add a ParisianCategory detail template"""
+        pass
+
+
+class CategoryTests(GenericCRUDTestMixin, TestCase):
+    model = Category
+    url_names = {'list': 'categories'}
+
+    def get_add_form_data(self):
+        parent = Category.objects.first()
+        catalogue = parent.catalogue
+        parisian_category, create = ParisianCategory.objects.get_or_create(**ParisianCategoryTests().get_add_form_data())
+        return {
+            'catalogue': catalogue,
+            'parent': parent,
+            'bookseller_category': 'bookseller_category test',
+            'parisian_category': parisian_category
+        }
+
+    def get_change_form_data(self):
+        return {
+            'bookseller_category': 'bookseller_category test2'
+        }
+
+    def test_Detail(self):
+        """TODO: add a ParisianCategory detail template"""
+        pass
+
+
