83481c8c5899d98851815066cb0c754b361e8a76
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2019-04-29 13:02:08 +0200

    Extended backup script with quaterly, montly, weekly, daily strategie. Also rsync for media files.

--- make_backups.sh
+++ make_backups.sh
@@ -1,22 +1,69 @@
 #!/bin/bash
 
-if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] || [ $1 == "--help" ] || [ $1 == "-h" ]
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] || [ -z $4 ] || [ -z $5 ] || [ $1 == "--help" ] || [ $1 == "-h" ]
 then
-    echo "Usage: $(basename $0) WORKING-DIR TARGET-DIR VIRTUALENV-DIR"
+    echo "Usage: $(basename $0) WORKING-DIR TARGET-DIR VIRTUALENV-DIR MEDIA_SOURCE_DIR MEDIA_BACKUP_DIR"
     echo ""
     echo "  WORKING-DIR       Directory where the backup command should be run"
     echo "  TARGET-DIR        Directory where the backup file should be stored"
-    echo "  VIRTUALENV-DIR    Directory of virtualenv for the backup command; may be"
-    echo "                    relative to WORKING-DIR"
+    echo "  VIRTUALENV-DIR    Directory of virtualenv for the backup command; may be relative to WORKING-DIR"
+    echo "  MEDIA_SOURCE_DIR  Directory containing the media files to be backed up; may be relative to WORKING-DIR"
+    echo "  MEDIA_BACKUP_DIR  Directory for backing up media files; may be relative to WORKING-DIR"
     exit 1
 fi
 
 WORKING_DIR=$1
-BACKUP_DIR=$2
+BACKUP_MAIN_DIR=$2
 VIRTUALENV_DIR=$3
-DBBACKUP_FILE=backup.sql.gz
+MEDIA_SOURCE_DIR=$4
+MEDIA_BACKUP_DIR=$5
 
-mkdir -p $BACKUP_DIR &&
+FILENAME_PREFIX=$(date +%Y-%m-%d_%H.%M.%S)
+
+mkdir -p $BACKUP_MAIN_DIR/logs
+BACKUP_LOG=$BACKUP_MAIN_DIR/logs/backup_${FILENAME_PREFIX}.log
+
+# Determine the type of backup: quarterly, monthly, weekly or daily
+DAYOFMONTH=$(date +%d)
+if [ $DAYOFMONTH == 01 ]; then
+    MONTH=$(date +%m)
+    if [ $MONTH == 01 ] || [ $MONTH == 04 ] || [ $MONTH == 07 ] || [ $MONTH == 10 ]; then
+        # Quarterly. KEEP!!
+        BACKUP_SUBDIR=quaterly
+        MAX_NUMBER_OF_FILES=-1;
+    else
+        BACKUP_SUBDIR=monthly;
+        MAX_NUMBER_OF_FILES=8;
+    fi
+else
+    DAYOFWEEK=$(date +%w)
+    if [ $DAYOFWEEK == 0 ]; then
+        BACKUP_SUBDIR=weekly;
+        MAX_NUMBER_OF_FILES=5;
+    else
+        BACKUP_SUBDIR=daily;
+        MAX_NUMBER_OF_FILES=6;
+    fi
+fi
+
+# Create the backup directory if it doesn't exist
+BACKUP_DIR=$BACKUP_MAIN_DIR/$BACKUP_SUBDIR
+mkdir -p $BACKUP_DIR
+
+# Delete the oldest backups if the number of backups exceed the max
+echo -e "\n--- Deleting obsolete database backups ---" | tee -a $BACKUP_LOG
+if [ $MAX_NUMBER_OF_FILES -gt 0 ]; then
+    ls -A1t $BACKUP_DIR | tail -n +$MAX_NUMBER_OF_FILES | tee -a /dev/fd/2 $BACKUP_LOG | xargs -i --no-run-if-empty rm $BACKUP_DIR/{};
+fi
+
+DBBACKUP_FILE=${FILENAME_PREFIX}_backup.sql.gz
+
+# Backup database
+echo -e "\n--- Backing up the database ---" | tee -a $BACKUP_LOG
 cd $WORKING_DIR &&
 . $VIRTUALENV_DIR/bin/activate &&
-python manage.py dbbackup -z -O $BACKUP_DIR/$DBBACKUP_FILE
+python manage.py dbbackup --noinput -z -O $BACKUP_DIR/$DBBACKUP_FILE 2>&1 | tee -a $BACKUP_LOG
+
+# Backup media file
+echo -e "\n--- Backing up media files ---" | tee -a $BACKUP_LOG
+rsync -av $MEDIA_SOURCE_DIR $MEDIA_BACKUP_DIR | tee -a $BACKUP_LOG
