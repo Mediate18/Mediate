425b6b9a901a6c63adc68dde271f4d9bfe062408
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2019-05-27 16:17:51 +0200

    Removed the 'output as JSON' because is didn't work as planned.

--- mediate/management/commands/import_spi_data.py
+++ mediate/management/commands/import_spi_data.py
@@ -10,22 +10,12 @@ Import SPi data from an existing database
 """
 
 from django.core.management.base import BaseCommand
+from django.db import transaction
 import sqlite3
-import json
-from django.core import serializers
 import re
 import catalogues
 import items
 import persons
-
-
-# Get or new
-def get_or_new(model, **values):
-    from django.core.exceptions import ObjectDoesNotExist
-    try:
-        return model.objects.get(**values), False
-    except ObjectDoesNotExist:
-        return model(**values), True
 
 
 class Command(BaseCommand):
@@ -38,25 +28,17 @@ class Command(BaseCommand):
         # Optional
         parser.add_argument('-c', '--catalogue_ids_filename', type=str,
                             help='Path to a list of catalogue IDs to process (one ID per line)')
-        parser.add_argument('-p', '--pretty_print', action='store_true',
-                            help='Pretty print the JSON output')
-        parser.add_argument('-s', '--save_new_objects', action='store_true',
-                            help='Save new objects in the database')
 
+    @transaction.atomic
     def handle(self, *args, **kwargs):
         # Get the command line arguments
         database_path = kwargs['database_path']
         catalogue_ids_filename = kwargs['catalogue_ids_filename']
-        pretty_print = kwargs['pretty_print']
-        save_new_objects = kwargs['save_new_objects']
 
         # Read catalogue IDs
         if catalogue_ids_filename:
             with open(catalogue_ids_filename, 'r') as cat_ids_file:
                 catalogue_ids = [id.strip() for id in cat_ids_file.readlines()]
-
-        # List of new objects to serialize
-        self.new_objects = []
 
         # Open database connections
         def dict_factory(cursor, row):
@@ -64,52 +46,37 @@ class Command(BaseCommand):
             for idx, col in enumerate(cursor.description):
                 d[col[0]] = row[idx]
             return d
-
         db_connection = sqlite3.connect(database_path)
         db_connection.row_factory = dict_factory
-        dictCursor = db_connection.cursor()
+        cursor = db_connection.cursor()
 
-        dummy_collection, new = get_or_new(catalogues.models.Collection, name='Dummy collection')
-        if new:
-            self.new_objects.append(dummy_collection)
+        dummy_collection, created = catalogues.models.Collection.objects.get_or_create(name='Dummy collection')
 
         # Catalogues
-        catalogue_id_mapping = self.create_catalogues(dummy_collection, catalogue_ids, dictCursor)
+        catalogue_id_mapping = self.create_catalogues(dummy_collection, catalogue_ids, cursor)
 
         # Minimal Lot ID per Catalogue
-        dictCursor.execute("SELECT catalogue_id, MIN(id) FROM `lot` GROUP BY catalogue_id")
+        cursor.execute("SELECT catalogue_id, MIN(id) FROM `lot` GROUP BY catalogue_id")
         minimal_lot_id_per_catalogue = dict([
             (row['catalogue_id'], row['MIN(id)'])
-            for row in dictCursor.fetchall()
+            for row in cursor.fetchall()
         ])
 
         # Lots
-        lot_id_mapping, lot_ids = self.create_lots(catalogue_id_mapping, catalogue_ids, dictCursor,
+        lot_id_mapping, lot_ids = self.create_lots(catalogue_id_mapping, catalogue_ids, cursor,
                                                    minimal_lot_id_per_catalogue)
 
         # Items
-        self.create_items(dictCursor, dummy_collection, lot_id_mapping, lot_ids)
+        self.create_items(cursor, dummy_collection, lot_id_mapping, lot_ids)
 
         # Close database connection
-        dictCursor.close
+        cursor.close
         db_connection.close()
 
-        if save_new_objects:
-            for obj in self.new_objects:
-                obj.save()
-
-        # Output the new objects as a list of serialized objects
-        json_str = serializers.serialize("json", self.new_objects)
-        if pretty_print:
-            parsed = json.loads(json_str)
-            print(json.dumps(parsed, indent=4, sort_keys=True))
-        else:
-            print(json_str)
-
-    def create_items(self, dictCursor, dummy_collection, lot_id_mapping, lot_ids):
-        dictCursor.execute("SELECT `item`.*, `lot`.entry_text FROM `item` JOIN `lot` ON `item`.lot_id = `lot`.id "
+    def create_items(self, cursor, dummy_collection, lot_id_mapping, lot_ids):
+        cursor.execute("SELECT `item`.*, `lot`.entry_text FROM `item` JOIN `lot` ON `item`.lot_id = `lot`.id "
                            "WHERE `lot`.id IN({})".format(",".join([str(id) for id in lot_ids])))
-        resultSet = dictCursor.fetchall()
+        resultSet = cursor.fetchall()
         for row in resultSet:
             places_of_publication = re.compile(r'\/').split(row['place_of_publication'])
             # print('places_of_publication: ' + str(places_of_publication))
@@ -124,22 +91,20 @@ class Command(BaseCommand):
             # print('date: ' + str(date))
 
             if places_of_publication and places_of_publication[0]:
-                place, new = get_or_new(persons.models.Place, name=places_of_publication[0], cerl_id=None)
+                place, created = persons.models.Place.objects.get_or_create(name=places_of_publication[0], cerl_id=None)
             else:
                 place = None
             # print('place: ' + str(place))
-            person, new = get_or_new(persons.models.Person, short_name=row['publisher'],
+            person, created = persons.models.Person.objects.get_or_create(short_name=row['publisher'],
                                      surname=row['publisher'],
                                      first_names='',
                                      sex='UNKNOWN')
-            if new:
-                self.new_objects.append(person)
             # print('person: ' + str(person) + " " + str(created))
             edition = items.models.Edition(year=date, place=place)
-            self.new_objects.append(edition)
+            edition.save()
             # print('edition: ' + str(edition))
             publisher = items.models.Publisher(edition=edition, publisher=person)
-            self.new_objects.append(publisher)
+            publisher.save()
             # print('publisher: ' + str(publisher))
 
             item_entries = re.compile(r'\s?\/\s?').split(row['entry_text'])
@@ -151,9 +116,7 @@ class Command(BaseCommand):
             try:
                 book_format = None
                 if row['book_format']:
-                    book_format, new = get_or_new(items.models.BookFormat, name=row['book_format'])
-                    if new:
-                        self.new_objects.append(book_format)
+                    book_format, created = items.models.BookFormat.objects.get_or_create(name=row['book_format'])
                 insert_fields = {
                     'short_title': short_title[:128],
                     'lot_id': lot_id_mapping[row['lot_id']],
@@ -170,27 +133,25 @@ class Command(BaseCommand):
 
             try:
                 item = items.models.Item(**insert_fields)
-                self.new_objects.append(item)
+                item.save()
             except Exception as e:
                 print(insert_fields)
                 raise e
 
             if row['binding_material_details']:
-                material_details, new = get_or_new(items.models.MaterialDetails,
-                                                   description=row['binding_material_details'])
-                if new:
-                    self.new_objects.append(material_details)
+                material_details, created = items.models.MaterialDetails.objects.get_or_create(
+                    description=row['binding_material_details'])
                 item_materialdetails_relations = items.models.ItemMaterialDetailsRelation(item=item,
                                                                                           material_details=material_details)
-                self.new_objects.append(item_materialdetails_relations)
+                item_materialdetails_relations.save()
 
-    def create_lots(self, catalogue_id_mapping, catalogue_ids, dictCursor, minimal_lot_id_per_catalogue):
+    def create_lots(self, catalogue_id_mapping, catalogue_ids, cursor, minimal_lot_id_per_catalogue):
         lot_id_mapping = {}
         if catalogue_ids:
-            dictCursor.execute("SELECT * FROM lot WHERE catalogue_id IN({})".format(",".join(catalogue_ids)))
+            cursor.execute("SELECT * FROM lot WHERE catalogue_id IN({})".format(",".join(catalogue_ids)))
         else:
-            dictCursor.execute("SELECT * FROM lot")
-        resultSet = dictCursor.fetchall()
+            cursor.execute("SELECT * FROM lot")
+        resultSet = cursor.fetchall()
         lot_ids = []
         for row in resultSet:
             try:
@@ -221,15 +182,11 @@ class Command(BaseCommand):
                 child = bookseller_category
 
             # Link to Category
-            parent_category, new = get_or_new(catalogues.models.Category, catalogue_id=catalogue_id,
+            parent_category, created = catalogues.models.Category.objects.get_or_create(catalogue_id=catalogue_id,
                                               bookseller_category=parent) if parent else (None, None)
-            if new:
-                self.new_objects.append(parent_category)
 
-            category, new = get_or_new(catalogues.models.Category, catalogue_id=catalogue_id,
+            category, created = catalogues.models.Category.objects.get_or_create(catalogue_id=catalogue_id,
                                        bookseller_category=child, parent=parent_category)
-            if new:
-                self.new_objects.append(category)
 
             insert_fields = {
                 'catalogue_id': catalogue_id,
@@ -243,7 +200,7 @@ class Command(BaseCommand):
 
             try:
                 lot = catalogues.models.Lot(**insert_fields)
-                self.new_objects.append(lot)
+                lot.save()
                 lot_id_mapping[row['id']] = lot.pk
             except Exception as e:
                 print(insert_fields)
@@ -251,18 +208,15 @@ class Command(BaseCommand):
 
         return lot_id_mapping, lot_ids
 
-    def create_catalogues(self, dummy_collection, catalogue_ids, dictCursor):
+    def create_catalogues(self, dummy_collection, catalogue_ids, cursor):
         catalogue_id_mapping = {}
         if catalogue_ids:
             query = "SELECT * FROM catalogue WHERE id IN({})".format(",".join(catalogue_ids))
         else:
             query = "SELECT * FROM catalogue"
-        dictCursor.execute(query)
-        resultSet = dictCursor.fetchall()
+        cursor.execute(query)
+        resultSet = cursor.fetchall()
         for row in resultSet:
-            # Deal with city first
-            # Nothing to do yet - city is dealt with in Publication
-
             insert_fields = {
                 'short_title': row['short_title'],
                 'full_title': row['full_title'],
@@ -272,7 +226,7 @@ class Command(BaseCommand):
 
             try:
                 catalogue = catalogues.models.Catalogue(**insert_fields)
-                self.new_objects.append(catalogue)
+                catalogue.save()
                 catalogue_id_mapping[row['id']] = catalogue.pk
             except Exception as e:
                 print(insert_fields)
