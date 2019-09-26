67589719fc38852de54e713a29b944fe7c0d6a6c
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2019-09-26 09:23:26 +0200

    Made dashboard load faster for non-super users.

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,13 +4,17 @@ from django.db.models import Count
 from items.models import Item, Edition
 
 def view_dashboard(request):
-    number_of_editions_without_items = Edition.objects.annotate(number_of_items=Count('items'))\
-        .filter(number_of_items=0).count() or 0
-    number_of_editions_gt_1_item = Edition.objects.annotate(number_of_items=Count('items'))\
-                                             .filter(number_of_items__gt=1).count() or 0
-    number_of_items_without_editions = Item.objects.filter(edition__isnull=True).count() or 0
-    return render(request, 'dashboard/dashboard.html', {
-        'number_of_editions_without_items': number_of_editions_without_items,
-        'number_of_editions_gt_1_item': number_of_editions_gt_1_item,
-        'number_of_items_without_editions': number_of_items_without_editions
-    })
+    if request.user.is_superuser:
+        number_of_editions_without_items = Edition.objects.annotate(number_of_items=Count('items'))\
+            .filter(number_of_items=0).count() or 0
+        number_of_editions_gt_1_item = Edition.objects.annotate(number_of_items=Count('items'))\
+                                                 .filter(number_of_items__gt=1).count() or 0
+        number_of_items_without_editions = Item.objects.filter(edition__isnull=True).count() or 0
+        context = {
+            'number_of_editions_without_items': number_of_editions_without_items,
+            'number_of_editions_gt_1_item': number_of_editions_gt_1_item,
+            'number_of_items_without_editions': number_of_items_without_editions
+        }
+    else:
+        context = {}
+    return render(request, 'dashboard/dashboard.html', context)
