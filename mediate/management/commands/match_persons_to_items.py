ddc0a77da8313495cc17aea499026e6bca6e729d
Micha Hulsbosch <m.hulsbosch@let.ru.nl>
2020-01-21 11:00:28 +0100

    match_persons_to_items command, version 4

--- mediate/management/commands/match_persons_to_items.py
+++ mediate/management/commands/match_persons_to_items.py
@@ -5,6 +5,7 @@ Match Persons to Items
 
 
 import re
+from collections import defaultdict
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.db.models import Count, Subquery
@@ -41,7 +42,7 @@ class Command(BaseCommand):
         self.do_matching(catalogue_ids)
 
     def normalize(self, string):
-        return re.sub(r' +', ' ', string['short_title'].replace(":", ".").replace(",", ".").lower())
+        return re.sub(r' +', ' ', string.replace(":", ".").replace(",", ".").lower())
 
     # Yield successive n-sized
     # chunks from l.
@@ -63,42 +64,71 @@ class Command(BaseCommand):
         return duplicates
 
     def do_matching(self, catalogue_ids):
-        # Determine duplicate short_titles
-        short_titles = [(item['uuid'], self.normalize(item))
-                        for item in Item.objects.values('short_title', 'uuid')]
+        """Matches normalized short_titles and copies the PersonItemRelations."""
 
-        print("#short_titles:", len(short_titles), short_titles[0])
+        short_titles_with_relations = self.get_short_titles_with_relations()
+        print("#short_titles_with_relations", len(short_titles_with_relations))
 
-        short_title_duplicates = self.duplicates(short_titles)
+        short_titles_target_items = self.get_short_titles_from_selected_catalogues(catalogue_ids)
+        print("#short_titles_target_items", len(short_titles_target_items))
 
-        print("#duplicates:", len(short_title_duplicates), len(set(short_title_duplicates)))
+        # Find short_titles that are in both lists
+        short_title_intersection = short_titles_with_relations.keys() & short_titles_target_items.keys()
+        print("#short_title_intersection", len(short_title_intersection))
+        total_items_with_copyable_relations = 0
+        for short_title in short_title_intersection:
+            total_items_with_copyable_relations += len(short_titles_with_relations[short_title])
+        print("total_items_with_copyable_relations:", total_items_with_copyable_relations)
 
-        total = 0
+        # Copy PersonItemRelations to target items
+        grand_total = 0
 
-        for duplicate_chunk in self.divide_chunks(short_title_duplicates, 1000):
+        for short_title in short_title_intersection:
+            personitemrelations_to_copy = PersonItemRelation.objects\
+                .filter(item_id__in=short_titles_with_relations[short_title])
+            total_for_short_title = 0
+            for relation_to_copy in personitemrelations_to_copy:
+                for item_id in short_titles_target_items[short_title]:
+                    relation, created = PersonItemRelation.objects.get_or_create(item_id=item_id,
+                                                                        person=relation_to_copy.person,
+                                                                        role=relation_to_copy.role)
+                    if created:
+                        total_for_short_title += 1
+            print("Total for {}: {}".format(short_title, total_for_short_title))
+            grand_total += total_for_short_title
 
-            duplicates_with_personitemrelations = Item.objects\
-                .filter(uuid__in=duplicate_chunk)\
-                .annotate(personitemrelation_cnt=Count('personitemrelation'))\
-                .order_by()\
-                .filter(personitemrelation_cnt__gt=0)
+        print("Grand total:", grand_total)
 
-            print("#relations:", PersonItemRelation.objects.filter(item__uuid__in=duplicate_chunk).count())
+    def get_short_titles_from_selected_catalogues(self, catalogue_ids):
+        """Get short_titles from items that are in a selection of catalogues"""
+        
+        items_from_selected_catalogues = Item.objects \
+            .values_list('short_title', 'uuid') \
+            .annotate(personitemrelation_cnt=Count('personitemrelation')) \
+            .filter(lot__catalogue__short_title__in=catalogue_ids) \
+            .order_by() \
+            .filter(personitemrelation_cnt=0)
 
-            for source_item in duplicates_with_personitemrelations:
-                personitemrelations_to_copy = PersonItemRelation.objects.filter(item=source_item)
+        short_titles_from_selected_catalogues = defaultdict(list)
+        for short_title, uuid, cnt in items_from_selected_catalogues:
+            short_titles_from_selected_catalogues[self.normalize(short_title)].append(uuid)
 
-                relation_count = personitemrelations_to_copy.count()
-                # print("Count:", relation_count)
-                total += relation_count
+        return short_titles_from_selected_catalogues
 
-                # for target_item in Item.objects.filter(short_title=source_item.short_title,
-                #                                        lot__catalogue__short_title__in=catalogue_ids):
-                    # for relation in personitemrelations_to_copy:
-                    #     PersonItemRelation(item=target_item, person=relation.person, role=relation.role).save()
+    def get_short_titles_with_relations(self):
+        """Get short_titles from items that also have a relation to a person"""
 
-        print("Total:", total)
+        items_with_relations = Item.objects \
+            .values_list('short_title', 'uuid') \
+            .annotate(personitemrelation_cnt=Count('personitemrelation')) \
+            .order_by() \
+            .filter(personitemrelation_cnt__gt=0)
 
+        short_titles_with_relations = defaultdict(list)
+        for short_title, uuid, cnt in items_with_relations:
+            short_titles_with_relations[self.normalize(short_title)].append(uuid)
+
+        return short_titles_with_relations
 
 
 
