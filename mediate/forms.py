4afc28c04af2b61e78516a459f70d02f28365d46
Micha Hulsbosch <michahulsbosch@gmail.com>
2021-06-21 10:53:01 +0200

    Make Dataset select use default dataset if none selected.

--- mediate/forms.py
+++ mediate/forms.py
@@ -3,6 +3,7 @@ from django_select2.forms import ModelSelect2MultipleWidget
 from guardian.shortcuts import get_objects_for_user
 
 from catalogues.models import Dataset
+from catalogues.tools import get_dataset_for_anonymoususer
 
 class SelectDatasetForm(forms.Form):
     def __init__(self, *args, **kwargs):
@@ -11,8 +12,15 @@ class SelectDatasetForm(forms.Form):
         super(SelectDatasetForm, self).__init__(*args, **kwargs)
 
         datasets = get_objects_for_user(self.request.user, 'catalogues.change_dataset', Dataset)
+
+        if 'datasets' in self.request.session:
+            selected_datasets = [dataset['uuid'] for dataset in self.request.session['datasets']]
+        else:
+            selected_datasets = [get_dataset_for_anonymoususer()]
+
+
         self.fields['datasets'] = forms.ModelMultipleChoiceField(
-            initial=[dataset['uuid'] for dataset in self.request.session['datasets']]  ,
+            initial=selected_datasets,
             queryset=datasets,
             widget=ModelSelect2MultipleWidget(queryset=datasets, search_fields=['name__icontains'])
         )
