7c5bdcb7ff7111093e9c6af94041b281c768e8f1
Micha Hulsbosch <michahulsbosch@gmail.com>
2022-03-28 15:57:32 +0200

    Make the Edition-Item links and Item-Lot links use datasets

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,19 +3,28 @@ from django.db.models import Count, Q
 
 from items.models import Item, Edition, Work, Language, PersonItemRelation, ItemWorkRelation
 from catalogues.models import Lot, Catalogue
+from catalogues.views.views import get_catalogues_for_session
 from persons.models import Place, Person, Country
 
 def view_dashboard(request):
     if request.user.is_superuser:
-        number_of_editions_without_items = Edition.objects.annotate(number_of_items=Count('items'))\
-            .filter(number_of_items=0).count() or 0
-        number_of_editions_gt_1_item = Edition.objects.annotate(number_of_items=Count('items'))\
-                                                 .filter(number_of_items__gt=1).count() or 0
-        number_of_items_without_editions = Item.objects.filter(edition__isnull=True).count() or 0
+        number_of_editions_without_items = Edition.objects\
+                                               .filter(items__lot__catalogue__in=get_catalogues_for_session(request))\
+                                               .annotate(number_of_items=Count('items'))\
+                                               .filter(number_of_items=0).count() or 0
+        number_of_editions_gt_1_item = Edition.objects\
+                                           .filter(items__lot__catalogue__in=get_catalogues_for_session(request))\
+                                           .annotate(number_of_items=Count('items'))\
+                                           .filter(number_of_items__gt=1).count() or 0
+        number_of_items_without_editions = Item.objects.filter(lot__catalogue__in=get_catalogues_for_session(request),
+                                                               edition__isnull=True).count() or 0
 
-        number_of_items_without_lot = Item.objects.filter(lot__isnull=True).count() or 0
-        number_of_lots_without_items = Lot.objects.annotate(number_of_items=Count('item')).filter(number_of_items=0)\
-            .count() or 0
+        number_of_items_without_lot = Item.objects.filter(lot__catalogue__in=get_catalogues_for_session(request),
+                                                          lot__isnull=True).count() or 0
+        number_of_lots_without_items = Lot.objects\
+                                           .filter(catalogue__in=get_catalogues_for_session(request))\
+                                           .annotate(number_of_items=Count('item')).filter(number_of_items=0)\
+                                           .count() or 0
 
         context = {
             'number_of_editions_without_items': number_of_editions_without_items,
