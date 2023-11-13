6021daa2db8b975f465f8bac6584ef4993b25fa5
Micha Hulsbosch <michahulsbosch@gmail.com>
2023-11-13 13:47:10 +0100

    feat: Change Edition.place to on_delete=SET_NULL

--- items/models.py
+++ items/models.py
@@ -344,7 +344,7 @@ class Edition(models.Model):
     year_end = models.IntegerField(_("Year of publication - end"), null=True, blank=True)
     year_tag = models.CharField(_("Year of publication tag"), max_length=128, blank=True)
     terminus_post_quem = models.BooleanField(_("Terminus post quem"), default=False)
-    place = models.ForeignKey(Place, on_delete=models.PROTECT, null=True, blank=True)
+    place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, blank=True)
     url = models.CharField(_("URL"), max_length=1024, blank=True)
 
     def __str__(self):
