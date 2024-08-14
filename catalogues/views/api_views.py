b627d8d3bec08e01cb84374a13d77d923260f108
Micha Hulsbosch <michahulsbosch@gmail.com>
2024-08-14 11:24:29 +0200

    feat: Add filter/search to API

--- catalogues/views/api_views.py
+++ catalogues/views/api_views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django.http import Http404
+from rest_framework import viewsets, filters
 from ..serializers import *
 
 
@@ -6,12 +7,31 @@ class DatasetViewSet(viewsets.ModelViewSet):
     queryset = Dataset.objects.all()
     serializer_class = DatasetSerializer
     http_method_names = ['get']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
 
 class CollectionViewSet(viewsets.ModelViewSet):
     queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
     http_method_names = ['get']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['short_title']
+
+    filter_fields = ['short_title', 'year_of_publication']
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Collection.objects.all()
+        for param, value in self.request.query_params.items():
+            if param.split('__')[0] in self.filter_fields:
+                queryset = queryset.filter(**{param: value})
+            else:
+                raise Http404
+        return queryset
 
 
 class CollectionTypeViewSet(viewsets.ModelViewSet):
@@ -24,6 +44,8 @@ class CatalogueViewSet(viewsets.ModelViewSet):
     queryset = Catalogue.objects.all()
     serializer_class = CatalogueSerializer
     http_method_names = ['get']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
 
 class LotViewSet(viewsets.ModelViewSet):
--- mediate/settings.py
+++ mediate/settings.py
@@ -229,7 +229,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'URL_FIELD_NAME': 'api-url'
+    'URL_FIELD_NAME': 'api-url',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 SITE_ID = 1
