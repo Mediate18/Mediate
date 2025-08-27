17751f5f3d5a039af83696dc79f434e97874ada0
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-08-27 10:24:58 +0200

    feat: Add CSRF_TRUSTED_ORIGINS envvar

--- mediate/settings.py
+++ mediate/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
 
 ADMINS = [
     ('Micha Hulsbosch', 'm.hulsbosch@let.ru.nl')
