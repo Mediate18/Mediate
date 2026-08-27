61fd13975eac3cc4b8cd2efe0bb0ad554e64ab41
Micha Hulsbosch <michahulsbosch@gmail.com>
2026-08-27 13:52:52 +0200

    Remove OldHostNameWarningMiddleware when testing

--- mediate/settings_tests.py
+++ mediate/settings_tests.py
@@ -8,3 +8,5 @@ if TESTING:
     PASSWORD_HASHERS = [
         'django.contrib.auth.hashers.MD5PasswordHasher',
     ]
+
+MIDDLEWARE = [middelware for middelware in MIDDLEWARE if middelware != 'mediate.middleware.OldHostNameWarningMiddleware']
